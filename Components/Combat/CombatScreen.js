b307ef94048623c510de0a5b36b3e02fa25ce826
Thomasvr14 <thomas_vr@hotmail.nl>
2023-06-26 17:46:36 +0200

    added health counters

--- Components/Combat/CombatScreen.js
+++ Components/Combat/CombatScreen.js
@@ -8,6 +8,7 @@ import StatSection from "./StatSection";
 import { useState, useEffect } from "react";
 import ArmorSection from "./ArmorSection";
 import { AbilityScoreData } from "../AbilityScoreData";
+import HitPointSection from "./HitPointSection";
 
 export default function CombatScreen({ abilityScoreData }) {
   const [abilityScores, setAbilityScores] = useState(AbilityScoreData);
@@ -25,32 +26,7 @@ export default function CombatScreen({ abilityScoreData }) {
     <LinearGradient colors={Colors.basicBackground} style={styles.container}>
       <ScrollView>
         <StatSection abilityScores={abilityScores} armorClass={armorClass} />
-        <View style={styles.healthContainer}>
-          <View style={styles.currentHPContainer}>
-            <View style={styles.hpTop}>
-              <Text style={styles.hpMaxTitle}>Hit Point Maximum: </Text>
-              <Text style={styles.hpMax}>30</Text>
-            </View>
-            <View style={styles.healthCounterContainer}>
-              <Text style={styles.buttonLeft}>-</Text>
-              <Text style={styles.healthCounter}>27</Text>
-              <Text style={styles.buttonRight}>+</Text>
-            </View>
-            <View>
-              <Text style={styles.healthTitle}>Current Hit Points</Text>
-            </View>
-          </View>
-          <View style={styles.temporaryHPContainer}>
-            <View style={styles.healthCounterContainer}>
-              <Text style={styles.buttonLeft}>-</Text>
-              <Text style={styles.healthCounter}>0</Text>
-              <Text style={styles.buttonRight}>+</Text>
-            </View>
-            <View>
-              <Text style={styles.healthTitle}>Temporary Hit Points</Text>
-            </View>
-          </View>
-        </View>
+        <HitPointSection />
         <Text style={styles.sectionTitle}>Weapons</Text>
         <WeaponSection abilityScores={abilityScores} />
         <Text style={styles.sectionTitle}>Armor</Text>
@@ -68,80 +44,6 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   equipmentContainer: {},
-  buttonLeft: {
-    borderBottomLeftRadius: 15,
-    borderTopLeftRadius: 15,
-    padding: 10,
-    paddingHorizontal: 12,
-    marginHorizontal: 10,
-    backgroundColor: Colors.button,
-    fontSize: 20,
-    borderWidth: 1,
-    elevation: 5,
-  },
-  buttonRight: {
-    borderBottomRightRadius: 15,
-    borderTopRightRadius: 15,
-    padding: 10,
-    marginHorizontal: 10,
-    backgroundColor: Colors.button,
-    fontSize: 20,
-    borderWidth: 1,
-    elevation: 5,
-  },
-  healthContainer: {
-    flex: 1,
-    padding: 10,
-    alignItems: "center",
-  },
-  currentHPContainer: {
-    backgroundColor: Colors.card,
-    borderWidth: 2,
-    borderTopStartRadius: 30,
-    borderTopEndRadius: 30,
-    elevation: 5,
-    padding: 10,
-    flex: 1.5,
-    justifyContent: "space-between",
-    width: "80%",
-  },
-  temporaryHPContainer: {
-    backgroundColor: Colors.card,
-    borderWidth: 2,
-    borderBottomStartRadius: 30,
-    borderBottomEndRadius: 30,
-    elevation: 5,
-    padding: 10,
-    marginTop: 10,
-    flex: 1,
-    width: "80%",
-  },
-  healthCounterContainer: {
-    flexDirection: "row",
-    justifyContent: "center",
-    paddingVertical: 20,
-    alignItems: "center",
-  },
-  hpMax: {
-    fontFamily: "Serif-Light",
-    color: "#0000008E",
-  },
-  hpMaxTitle: {
-    fontFamily: "Serif-Bold",
-    color: "#0000008E",
-  },
-  hpTop: {
-    flexDirection: "row",
-    fontFamily: "Serif-Light",
-  },
-  healthTitle: {
-    fontFamily: "Serif-Bold",
-    textAlign: "center",
-  },
-  healthCounter: {
-    fontFamily: "Serif-Light",
-    fontSize: 30,
-  },
   sectionTitle: {
     fontFamily: "Serif-Light-Italic",
     fontSize: 30,
---
+++ Components/Combat/HitPointSection.js
@@ -0,0 +1,205 @@
+import { StyleSheet, View, Text, TextInput } from "react-native";
+import Colors from "../../constants/Colors";
+
+import { useState, useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { TouchableOpacity } from "react-native-gesture-handler";
+
+const HitPointSection = () => {
+  const DATA = {
+    health: "",
+    maxHealth: "",
+    tempHealth: "",
+  };
+
+  const [health, setHealth] = useState(DATA);
+  const [refresh, setRefresh] = useState(false);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    const jsonValue = await AsyncStorage.getItem("health");
+
+    if (jsonValue != null) {
+      setHealth(JSON.parse(jsonValue));
+      setRefresh(!refresh);
+    }
+  };
+
+  useEffect(() => {
+    AsyncStorage.setItem("health", JSON.stringify(health));
+  }, [refresh]);
+
+  const onEditData = ({ value, prop }) => {
+
+    let copy = health;
+    copy[prop] = value;
+    setHealth(copy);
+    setRefresh(!refresh);
+  };
+
+  return (
+    <View style={styles.healthContainer}>
+      <View style={styles.currentHPContainer}>
+        <View style={styles.hpTop}>
+          <Text style={styles.hpMaxTitle}>Hit Point Maximum: </Text>
+          <TextInput
+            style={styles.hpMax}
+            onChangeText={(text) =>
+              onEditData({ value: text, prop: "maxHealth" })
+            }
+            defaultValue={health.maxHealth}
+          ></TextInput>
+        </View>
+        <View style={styles.healthCounterContainer}>
+          <TouchableOpacity
+            onPress={() =>
+              onEditData({
+                value: String(parseInt(health.health) - 1),
+                prop: "health",
+              })
+            }
+          >
+            <Text style={styles.buttonLeft}>-</Text>
+          </TouchableOpacity>
+          <TextInput
+            style={styles.healthCounter}
+            onChangeText={(text) => onEditData({ value: text, prop: "health" })}
+            defaultValue={health.health}
+          ></TextInput>
+          <TouchableOpacity
+            onPress={() =>
+              onEditData({
+                value: String(parseInt(health.health) + 1),
+                prop: "health",
+              })
+            }
+          >
+            <Text style={styles.buttonRight}>+</Text>
+          </TouchableOpacity>
+        </View>
+        <View>
+          <Text style={styles.healthTitle}>Current Hit Points</Text>
+        </View>
+      </View>
+      <View style={styles.temporaryHPContainer}>
+        <View style={styles.healthCounterContainer}>
+          <TouchableOpacity
+            onPress={() =>
+              onEditData({
+                value: String(parseInt(health.tempHealth) - 1),
+                prop: "tempHealth",
+              })
+            }
+          >
+            <Text style={styles.buttonLeft}>-</Text>
+          </TouchableOpacity>
+          <TextInput
+            style={styles.healthCounter}
+            onChangeText={(text) =>
+              onEditData({ value: text, prop: "tempHealth" })
+            }
+            defaultValue={health.tempHealth}
+          ></TextInput>
+          <TouchableOpacity
+            onPress={() =>
+              onEditData({
+                value: String(parseInt(health.tempHealth) + 1),
+                prop: "tempHealth",
+              })
+            }
+          >
+            <Text style={styles.buttonRight}>+</Text>
+          </TouchableOpacity>
+        </View>
+        <View>
+          <Text style={styles.healthTitle}>Temporary Hit Points</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default HitPointSection;
+
+const styles = StyleSheet.create({
+  buttonLeft: {
+    borderBottomLeftRadius: 15,
+    borderTopLeftRadius: 15,
+    padding: 10,
+    paddingHorizontal: 12,
+    marginHorizontal: 10,
+    backgroundColor: Colors.button,
+    fontSize: 20,
+    borderWidth: 1,
+    elevation: 5,
+  },
+  buttonRight: {
+    borderBottomRightRadius: 15,
+    borderTopRightRadius: 15,
+    padding: 10,
+    marginHorizontal: 10,
+    backgroundColor: Colors.button,
+    fontSize: 20,
+    borderWidth: 1,
+    elevation: 5,
+  },
+  healthContainer: {
+    flex: 1,
+    padding: 10,
+    alignItems: "center",
+  },
+  currentHPContainer: {
+    backgroundColor: Colors.card,
+    borderWidth: 2,
+    borderTopStartRadius: 30,
+    borderTopEndRadius: 30,
+    elevation: 5,
+    paddingTop: 5,
+    paddingHorizontal: 10,
+    paddingBottom: 10,
+    flex: 1.5,
+    justifyContent: "space-between",
+    width: "80%",
+  },
+  temporaryHPContainer: {
+    backgroundColor: Colors.card,
+    borderWidth: 2,
+    borderBottomStartRadius: 30,
+    borderBottomEndRadius: 30,
+    elevation: 5,
+    padding: 10,
+    marginTop: 10,
+    flex: 1,
+    width: "80%",
+  },
+  healthCounterContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    paddingVertical: 20,
+    alignItems: "center",
+  },
+  hpMax: {
+    fontFamily: "Serif-Light",
+    color: "#0000008E",
+  },
+  hpMaxTitle: {
+    fontFamily: "Serif-Bold",
+    color: "#0000008E",
+  },
+  hpTop: {
+    flexDirection: "row",
+    fontFamily: "Serif-Light",
+    alignItems: "center",
+  },
+  healthTitle: {
+    fontFamily: "Serif-Bold",
+    textAlign: "center",
+  },
+  healthCounter: {
+    fontFamily: "Serif-Light",
+    fontSize: 30,
+  },
+});
